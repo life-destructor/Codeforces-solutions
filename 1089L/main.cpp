ca9ae63ce85560d7183ec41a4f55d34f3dcc752f
Life-Destructor <ibrahimy@iastate.edu>
2026-01-09 23:13:31 -0600

    Solving problem 1089L

---
+++ 1089L/main.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    int n, k;
+    cin >> n >> k;
+    vector<int> jobs(n), time(n);
+    vector<int> fkingIdlers, thisIndex;
+
+    for (int i = 0; i < n; i++) {
+        cin >> jobs[i];
+        // cin >> time[i];
+        if (find(jobs.begin(), jobs.begin() + i, jobs[i]) != jobs.begin() + i) {
+            // this means this value has already been entered
+
+            thisIndex.push_back(i);
+            // fkingIdlers.push_back(time[i]);
+            // now with this, I know the times of all duplicates.
+        }
+    }
+    int temporary = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> time[i];
+        if (i == thisIndex[temporary]){
+            fkingIdlers.push_back(time[i]);
+            temporary++;
+        }
+    }
+    int counter = 0;
+    vector<int> numberOfPeople;
+    for (int i = 1; i <= k; i++) {
+        for (int j = 0; j < n; j++) {
+            if (jobs[j] == i) {
+                counter++;
+            }
+        }
+        numberOfPeople.push_back(counter);
+        // this array contains the number of people in each job.
+        counter = 0;
+    }
+    int temp = 1;
+    int numberOfJobsWithZeroIdlers = count(numberOfPeople.begin(),numberOfPeople.end(), 0);
+    sort(fkingIdlers.begin(), fkingIdlers.end());
+    int total = 0;
+    for (int i = 0; i < numberOfJobsWithZeroIdlers; i++) {
+        total += fkingIdlers[i];
+    }
+    cout << total;
+
+
+
+    // int numberOfJobsWithMoreIdlers = count_if(numberOfPeople.begin(), numberOfPeople.end(), [temp](int n) {
+    //     return n > 1;
+    // });
+
+    return 0;
+}
