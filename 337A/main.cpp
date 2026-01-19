992ba8c536aacb08d6a02258ce3bba2cb5ab3629
Life-Destructor <ibrahimy@iastate.edu>
2026-01-19 00:47:15 -0600

    Solved problem 337A

--- 337A/main.cpp
+++ 337A/main.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -7,11 +6,22 @@ int main()
 {
     int n, m;
     cin >> n >> m;
-    int f[m];
+    vector <int> f(m);
     for (int i = 0; i < m; i++) {
         cin >> f[i];
     }
-    sort (f, f+m);
-    cout << f[n-1] - f[0];
+    sort(f.begin(), f.end());
+    int minDiff = f[n-1] - f[0];
+
+    for (int i = 1; i <= m - n; i++) {
+        // The current window starts at 'i' and ends at 'i + n - 1'
+        int current_diff = f[i + n - 1] - f[i];
+        
+        // If this window has a smaller difference, update our answer
+        if (current_diff < minDiff) {
+            minDiff = current_diff;
+        }
+    }
+    cout << minDiff;
     return 0;
 }
