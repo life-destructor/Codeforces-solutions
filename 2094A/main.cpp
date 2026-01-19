a55e550b82a87543200bd6951b3a210b87ed382d
Life-Destructor <ibrahimy@iastate.edu>
2026-01-18 23:40:10 -0600

    Solved problem 2094A

---
+++ 2094A/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    int t; cin >> t;
+    while (t--) {
+        string firstWord, secondWord, thirdWord;
+        cin >> firstWord >> secondWord >> thirdWord;
+        cout << firstWord[0] << secondWord[0] << thirdWord[0] << "\n";
+    }
+    return 0;
+}
